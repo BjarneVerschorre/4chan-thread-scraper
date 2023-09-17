a8aa5c9e6a8de7a221dddcc63e8389f2e9e930f0
Bjarne <83133360+BjarneVerschorre@users.noreply.github.com>
2023-09-17 15:23:09 +0200

    More typing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import re
+import typing
 import asyncio
 import threading
 import httpx
@@ -7,19 +8,37 @@ import httpx
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def get_thread_info(url:str):
+
+# Types
+THREAD_POST = typing.TypedDict(
+    "ThreadPost", 
+    {
+       "no": int,
+       "com": str,
+       "sub": typing.NotRequired[str],
+       "tim": typing.NotRequired[int],
+       "ext": typing.NotRequired[str],
+
+    })
+URL = typing.NewType("URL", str)
+BOARD = typing.NewType("BOARD", str)
+THREAD_DATA = typing.NewType("THREAD_DATA", dict)
+
+def get_thread_info(url:URL) -> tuple[BOARD, URL]:
     """ Returns the board and thread info url from a 4chan thread url """
     a = re.match(r"https://boards.4chan.org/(\w+)/thread/(\d+)", url)
+    if not a or len(a.groups()) != 2:
+        raise ValueError("Invalid 4chan thread url")
     
-    board = a.group(1)
+    board = BOARD(a.group(1))
     thread_id = a.group(2)
-    thread_info_url = f"https://a.4cdn.org/{board}/thread/{thread_id}.json"
+    thread_info_url = URL(f"https://a.4cdn.org/{board}/thread/{thread_id}.json")
 
     return board, thread_info_url
 
-def attachment_url(board:str, filename:str):
+def attachment_url(board:BOARD, filename:str) -> URL:
     """ Returns the url of an attachment from a board and filename """
-    return f"https://i.4cdn.org/{board}/{filename}"
+    return URL(f"https://i.4cdn.org/{board}/{filename}")
 
 def save_attachment(attachment_data: bytes, path:str, file_name: str):
     with open(f"{path}/{file_name}", "wb") as f:
@@ -27,7 +46,7 @@ def save_attachment(attachment_data: bytes, path:str, file_name: str):
 
     print(f"\rDownloaded \"{file_name}\"", end="")
 
-async def download_attachment(client: httpx.AsyncClient, path:str, attachment_url:str):
+async def download_attachment(client: httpx.AsyncClient, path:str, attachment_url:URL):
     res = await client.get(attachment_url)
 
     if res.status_code != 200:
@@ -39,24 +58,28 @@ async def download_attachment(client: httpx.AsyncClient, path:str, attachment_ur
 
 
 async def main():
-    thread_url = input("4chan thread url: ")
+    thread_url = URL(input("4chan thread url: "))
 
     board, thread_info_url = get_thread_info(thread_url)
-    thread_data:dict = httpx.get(thread_info_url).json()
+    thread_data:THREAD_DATA = httpx.get(thread_info_url).json()
 
-    initial_post:dict = thread_data["posts"][0]
+    initial_post:THREAD_POST = thread_data["posts"][0]
     thread_id:int = initial_post["no"]
 
     # Sanitize thread name for folder name
     thread_name:str = initial_post.get("sub", initial_post.get("com", "Unnamed thread"))
-    thread_name = thread_name.replace("/", "-")
-    thread_name = re.match(r"[a-zA-Z0-9 -]+", thread_name).group(0).strip()
+    thread_name_match = re.match(r"[a-zA-Z0-9 -]+", thread_name)
+
+    if thread_name_match:
+        thread_name = thread_name_match.group(0)
+    else:
+        thread_name = "Unnamed thread"
  
-    thread_posts:list[dict] = thread_data.get("posts", [])
+    thread_posts:list[THREAD_POST] = thread_data.get("posts", [])
 
     attachment_urls = []
     for post in thread_posts:
-        if not "tim" in post:
+        if not "tim" in post or not "ext" in post:
             continue
         attachment_urls.append(attachment_url(board, str(post["tim"]) + post["ext"]))
 
